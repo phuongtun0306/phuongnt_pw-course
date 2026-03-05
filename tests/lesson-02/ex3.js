6ca88bad8e5367687f56d36acc479c2ad4f98f33
Phuong Nguyen <nguyenphuongtb3696@gmail.com>
2026-03-05 22:17:17 +0700

    feat: Them bai tap buoi 2

--- tests/lesson-02/ex3.js
+++ tests/lesson-02/ex3.js
@@ -1,2 +1,10 @@
 // a. a && b && c ->> trả về kết quả là false
 // b. a && b || c ->> trả về kết quả là true
+
+
+// new file:   tests/lesson-02/00-key-takeaways.md
+//         new file:   tests/lesson-02/01-lythuyet-git.txt
+//         new file:   tests/lesson-02/02-cacvung-git.txt 
+//         new file:   tests/lesson-02/ex1.js
+//         new file:   tests/lesson-02/ex2.js
+//         new file:   tests/lesson-02/ex3.js
